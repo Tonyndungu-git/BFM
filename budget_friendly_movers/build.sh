6884b80ce0c35fe1dd5d987e8ee0c272bc7e912f
Anthony Ndungu <anthonymsalami@gmail.com>
2024-04-12 20:42:54 +0300

    working on the build.sh file

--- budget_friendly_movers/build.sh
+++ budget_friendly_movers/build.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
-# exit on error
-
 set -o errexit
 
-pip install -r requirements.txt
-python manage.py collectstatic --no-input
-python manage.py migrate
+# Install Python dependencies
+pip install -r budget_friendly_movers/requirements.txt
+
+# Collect static files
+python budget_friendly_movers/manage.py collectstatic --no-input
+
+# Apply database migrations
+python budget_friendly_movers/manage.py migrate
